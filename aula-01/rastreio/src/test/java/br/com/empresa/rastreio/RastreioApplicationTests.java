3b9f419dc100f5d019cc012779978fce03296762
andre-cabral-veem <andre.cabral@veem.com>
2022-11-05 13:15:41 -0300

    initial commit

---
+++ aula-01/rastreio/src/test/java/br/com/empresa/rastreio/RastreioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.rastreio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastreioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
