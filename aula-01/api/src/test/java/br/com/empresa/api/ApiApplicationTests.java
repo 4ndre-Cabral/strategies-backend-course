f4ca6589e42c4d3496bfc063297d54f976113242
andre-cabral-veem <andre.cabral@veem.com>
2022-11-07 10:12:38 -0300

    initial commit

--- aula-01/api/src/test/java/br/com/empresa/api/ApiApplicationTests.java
+++ aula-01/api/src/test/java/br/com/empresa/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.empresa.rastreio;
+package br.com.empresa.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RastreioApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
