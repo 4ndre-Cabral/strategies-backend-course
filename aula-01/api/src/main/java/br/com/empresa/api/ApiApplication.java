fa770c9257a9dd1ca07ed11ea8546e45483b67a9
andre-cabral-veem <andre.cabral@veem.com>
2022-11-11 13:47:49 -0300

    update

--- aula-01/api/src/main/java/br/com/empresa/api/ApiApplication.java
+++ aula-01/api/src/main/java/br/com/empresa/api/ApiApplication.java
@@ -2,9 +2,6 @@ package br.com.empresa.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import br.com.empresa.api.config.FileStorageProperties;
 
 @SpringBootApplication
 public class ApiApplication {
