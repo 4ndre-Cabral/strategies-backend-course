eb21fad44d3fb4e080428190f122885141f1db35
andre.cabral <andre.cabral@cidadania.gov.br>
2022-11-15 18:13:40 -0300

    finalizado

--- aula-01/api/src/main/java/br/com/empresa/api/repository/EstudanteRepository.java
+++ aula-01/api/src/main/java/br/com/empresa/api/repository/EstudanteRepository.java
@@ -1,5 +1,7 @@
 package br.com.empresa.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +16,7 @@ public interface EstudanteRepository extends JpaRepository<Estudante, Long>{
 	
 
 	Page<Estudante> findAllByOrderByNomeDesc(Pageable pageable);
+	
+	List<Estudante> findByAvaliacaoCursosEstudanteIsNull();
 
 }
