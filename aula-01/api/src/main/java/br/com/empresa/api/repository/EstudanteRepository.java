d2ba12d123fd8b313968ca0d55ccbd880be58b23
andre.cabral <andre.cabral@cidadania.gov.br>
2022-11-20 21:29:19 -0300

    aula 7 completa

--- aula-01/api/src/main/java/br/com/empresa/api/repository/EstudanteRepository.java
+++ aula-01/api/src/main/java/br/com/empresa/api/repository/EstudanteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.empresa.api.entity.Estudante;
@@ -16,6 +17,16 @@ public interface EstudanteRepository extends JpaRepository<Estudante, Long>{
 	
 
 	Page<Estudante> findAllByOrderByNomeDesc(Pageable pageable);
+	
+	@Query(value = 	"SELECT e.* FROM api.estudante e " +
+					" left join api.avaliacao_curso ac ON ac.estudante_id = e.id " +
+					" where ac.estudante_id is null", nativeQuery = true)
+	List<Estudante> findByAvaliacaoCursosEstudanteIsNullNativeQuery();
+	
+	@Query(value = 	"SELECT e FROM Estudante e " +
+					" join AvaliacaoCurso ac " +
+					" where ac.estudante.id is null")
+	List<Estudante> findByAvaliacaoCursosEstudanteIsNullJPQL();
 	
 	List<Estudante> findByAvaliacaoCursosEstudanteIsNull();
 
